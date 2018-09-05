77d281261bdba408d10599895854507e5fce91f6
Richard Bao <richardbao419@gmail.com>
2018-09-05 03:04:43 -0400

    edit3

--- js/responsive.js
+++ js/responsive.js
@@ -1,27 +1,40 @@
-// $(document).ready(function() {
-// 	$('#fullpage').fullpage({
-//     sectionsColor: ['#f2f2f2', '#4BBFC3', 'lightskyblue', 'whitesmoke'],
-// 		//options here
-// 		autoScrolling:true,
-//     scrollOverflow:true,
-// 		scrollHorizontally: true
-// 	});
-//
-// });
-// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
 
-$('#fullpage').fullpage({
-  sectionsColor: ['#f2f2f2', 'lightskyblue', '#AFEEEE'],
-  //options here
-  autoScrolling:true,
-  scrollOverflow:true,
-  scrollHorizontally: true,
-  onLeave: function(index, nextIndex, direction){
-    if (direction == "down")  {
-      $('header').removeClass('nav-down').addClass('nav-up');
-    }
-    else if (direction == "up"){
-      $('header').removeClass('nav-up').addClass('nav-down');
-    }
+$(document).ready(function() {
+  $(window).scroll(function(event){
+      didScroll = true;
+  });
+
+  setInterval(function() {
+      if (didScroll) {
+          hasScrolled();
+          didScroll = false;
+      }
+  }, 250);
+
+  function hasScrolled() {
+      var st = $(this).scrollTop();
+
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+          // Scroll Down
+          $('header').removeClass('nav-down').addClass('nav-up');
+
+      } else {
+          // Scroll Up
+          if(st + $(window).height() < $(document).height()) {
+              $('header').removeClass('nav-up').addClass('nav-down');
+          }
+      }
+
+      lastScrollTop = st;
   }
 });
